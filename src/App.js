650fd2d2324564dd86538382e76bdbb4977de873
Praveen Sankhla <pk54451636465@gmail.com>
2022-02-19 16:48:39 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const [allEntry, setAllEntry]= useState([]);
+
+const submitform = (e) =>{
+  e.preventDefault();
+  const newEntry = {
+    email : email,
+    password : password
+  }
+
+  setAllEntry([...allEntry, newEntry]);
+  // console.log(allEntry)
+
+  if(newEntry.email=="pk54451636465@gmail.com"){
+    return(
+      <div>
+        <a href='https://collegeproject-27765.web.app/'></a>
+      </div>
+    )
+  }
+  else{
+    print("enter Valid login Info")
+  }
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <form action='' onSubmit={submitform}>
+    <div className='slider'>
+      <div className='container'>
+        <div className='headr'>
+          <img src='./img/01.png'></img>
+          <h1> SignUp</h1>
+        </div>
+        <div className='fom'>
+        <input type='email' placeholder='E-mail' name='email' id='email' value={email}
+        onChange={(e)=>setEmail(e.target.value)}></input>
+        <div className='tog'>
+        <input type={passwordShown ? "text" : "password"} placeholder='Password' id='password' value={password}
+        onChange={(e)=>setPassword(e.target.value)}>
+        </input>
+        <input type="checkbox" onClick={togglePassword} id='tg'></input>
+        </div>
+        <input type='submit' value='Submit' className='sub' id='submit'></input>
+        </div>
+      </div>
     </div>
+    </form>
+    </>
   );
 }
 
